519235c161cd5a29b042e720b33851ec723336f1
nilimamishra2002 <nilimamishra2002@gmail.com>
2025-10-31 22:05:46 +0530

    added changes

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT ;
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 // Connect to MongoDB
 // mongoose.connect(process.env.MONGO_URI, {
