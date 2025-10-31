02ab2883e9db79f5bed4898fb8ea4b94547277b7
nilimamishra2002 <nilimamishra2002@gmail.com>
2025-10-31 22:44:01 +0530

    added cors changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://sidlabs-expense-tracker-frontend.vercel.app"
+    "https://sidlabs-expense-tracker-frontend.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
