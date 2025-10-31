b112d70d7fc39a0a8bedda1998f788fc8114dfad
nilimamishra2002 <nilimamishra2002@gmail.com>
2025-10-31 21:02:13 +0530

    added changes

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log(err));
+mongoose.connect(process.env.ATLASDB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+.then(() => console.log('Connected to ATLAS DB'))
+.catch(err => console.error('DB connection error:', err));
 
 // console.log("MONGO_URI:", process.env.MONGO_URI);
 
