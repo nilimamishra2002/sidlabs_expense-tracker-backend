3914848d77db365c01c914ee8e0e976d0f7e9110
nilimamishra2002 <nilimamishra2002@gmail.com>
2025-10-31 23:51:56 +0530

    added cors changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://sidlabs-expense-tracker-frontend.vercel.app/"
+    "https://sidlabs-expense-tracker-frontend.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
